c3956007239e8a164459cfbc234d30914fd4f620
shihaoran <shihaoran.code@gmail.com>
2024-08-28 23:57:34 +0800

    add:blog-article

--- src/page/Home/home.js
+++ src/page/Home/home.js
@@ -1,33 +1,13 @@
 import { createElement } from "../../index"
 import './home.css'
 import useState from "../../../React/useState"
+import Articles from '../../compentens/articles/articles'
 /** @jsx createElement */
 const Home = () => {
     const [state, setState] = useState(0)
     return (
         <div className="container">
-            <div className="header">
-                <h1>My Blog</h1>
-                <p>Welcome to my blog!</p>
-            </div>
-            <div className="post">
-                <h2>First Post Title</h2>
-                <p>...........................................................................................</p>
-                <p className="meta">Posted on August 9, 2024</p>
-            </div>
-            <div className="post">
-                <h2>Second Post Title</h2>
-                <p>...........................................................................................</p>
-                <p className="meta">Posted on August 8, 2024</p>
-            </div>
-            <div className="post">
-                <h2>Third Post Title</h2>
-                <p>...........................................................................................</p>
-                <p className="meta">Posted on August 7, 2024</p>
-            </div>
-            <div className="footer">
-                <p>&copy; 2024 My Blog. </p>
-            </div>
+            <Articles />
         </div>
     )
 }
