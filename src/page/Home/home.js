c6fb2aec2f73cf6189d222d6de5a9ee37d920cf9
shihaoran <shihaoran.code@gmail.com>
2024-08-29 01:28:35 +0800

    add:Article

--- src/page/Home/home.js
+++ src/page/Home/home.js
@@ -2,12 +2,14 @@ import { createElement } from "../../index"
 import './home.css'
 import useState from "../../../React/useState"
 import ArticleOne from "../../docs/articleOne"
+import ArticleTwo from "../../docs/articleTwo"
 /** @jsx createElement */
 const Home = () => {
     const [state, setState] = useState(0)
     return (
         <div className="container">
             <ArticleOne />
+            <ArticleTwo />
         </div>
     )
 }
