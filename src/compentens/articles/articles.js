028954f0606a136d95a0218ffb65dfeacb517871
shihaoran <shihaoran.code@gmail.com>
2024-09-03 01:18:49 +0800

    fix:home

--- src/compentens/articles/articles.js
+++ src/compentens/articles/articles.js
@@ -4,34 +4,19 @@ import marked from 'marked'
 import useEffect from '../../../React/useEffect'
 import useState from '../../../React/useState'
 /** @jsx createElement */
-const Articles = () => {
-    const [issuesList, setIssueList] = useState(null)
-    useEffect(() => {
-        const fetchIssue = async () => {
-            const response = await fetch('https://api.github.com/repos/codeshihaoran/MiniReact/issues/1', {
-                headers: {
-                    'Authorization': `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`,
-                },
-            })
-            const data = await response.json()
-            setIssueList(data)
-        }
-        fetchIssue()
-    }, [])
-    console.log('666', issuesList);
-
+const Articles = ({ title, time, body }) => {
     return (
         <div className='article'>
             <h3 className="article-title">
                 <a href="#">
-                    <span></span>
+                    <span>{title}</span>
                 </a>
             </h3>
             <div className="article-top-meta">
                 <span className="posted-on">
                     <a href="#" rel="bookmark">
                         <time className="entry-date published" datetime="">
-
+                            {time}
                         </time>
                     </a>
                 </span>
@@ -39,27 +24,19 @@ const Articles = () => {
             <div className="article-content">
                 <div className="entry">
                     <h1 id="前言"><a href="#" className="headerlink" title="前言"></a>前言</h1>
-                    <p></p>
 
-                    <p></p>
                 </div>
             </div>
             <div className="article-footer">
                 <div className="article-meta pull-left">
                     <span className="post-categories">
                         <i className="icon-categories"></i>
-                        <a href="#"></a>
-                    </span>
-                    <span className="post-tags">
-                        <i className="icon-tags"></i>
-                        <a href="#"></a>
-                        <a href="#"></a>
-                        <a href="#"></a>
-                        <a href="#"></a>
+                        <a href="#">React</a>
                     </span>
                 </div>
             </div>
         </div>
     )
+
 }
 export default Articles
--- src/page/Home/home.js
+++ src/page/Home/home.js
@@ -1,14 +1,50 @@
 import { createElement } from "../../index"
 import './home.css'
 import useState from "../../../React/useState"
+import marked from 'marked'
+import useEffect from "../../../React/useEffect"
 import Articles from "../../compentens/articles/articles"
 /** @jsx createElement */
 const Home = () => {
-    const [state, setState] = useState(0)
-    return (
-        <div className="container">
-            <Articles />
-        </div>
-    )
+    const [issuesList, setIssueList] = useState([])
+    const a = [1, 2, 3]
+    useEffect(() => {
+        const fetchIssue = async () => {
+            const response = await fetch('https://api.github.com/repos/codeshihaoran/MiniReact/issues', {
+                headers: {
+                    'Authorization': `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`,
+                },
+            })
+            const data = await response.json()
+            setIssueList(data)
+        }
+        fetchIssue()
+    }, [])
+
+    if (issuesList && issuesList.length > 0) {
+        console.log(issuesList);
+
+        return (
+            <div className="container">
+                <Articles
+                    title={issuesList[0].title}
+                    time={issuesList[0].created_at}
+                    body={issuesList[0].body}
+                />
+                <Articles
+                    title={issuesList[1].title}
+                    time={issuesList[1].created_at}
+                    body={issuesList[1].body}
+                />
+                <Articles
+                    title={issuesList[2].title}
+                    time={issuesList[2].created_at}
+                    body={issuesList[2].body}
+                />
+            </div>
+        )
+
+    }
+
 }
 export default Home
